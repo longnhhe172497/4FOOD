82f891d751becfe38cb82bd1f64bf2126c250773
longnhhe172497 <longnhhe172497@fpt.edu.vn>
2024-09-24 15:33:17 +0700

    init project

---
+++ src/java/test.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HOANG LONG
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
