1d7894b16d0cc634f2101b88087f1a13608886cb
REPHOANG <hoangpdhe176197@fpt.edu.vn>
2024-10-02 08:05:47 +0700

    Update SignupStaff.java

--- src/java/entity/SignupStaff.java
+++ src/java/entity/SignupStaff.java
@@ -3,52 +3,23 @@ package entity;
 public class SignupStaff {
     
     // Fields for staff sign-up details
-    private String staffName;
     private String staffEmail;
-    private String staffID;
-    private String department;
     private String password;
 
     // Constructor
-    public SignupStaff(String staffName, String staffEmail, String staffID, String department, String password) {
-        this.staffName = staffName;
+    public SignupStaff(String staffEmail, String password) {
+
         this.staffEmail = staffEmail;
-        this.staffID = staffID;
-        this.department = department;
         this.password = password;
     }
 
     // Getter and Setter methods
-    public String getStaffName() {
-        return staffName;
-    }
-
-    public void setStaffName(String staffName) {
-        this.staffName = staffName;
-    }
-
     public String getStaffEmail() {
         return staffEmail;
     }
 
     public void setStaffEmail(String staffEmail) {
         this.staffEmail = staffEmail;
-    }
-
-    public String getStaffID() {
-        return staffID;
-    }
-
-    public void setStaffID(String staffID) {
-        this.staffID = staffID;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 
     public String getPassword() {
@@ -61,10 +32,7 @@ public class SignupStaff {
 
     // Method to display staff information
     public void displayStaffInfo() {
-        System.out.println("Staff Name: " + staffName);
         System.out.println("Staff Email: " + staffEmail);
-        System.out.println("Staff ID: " + staffID);
-        System.out.println("Department: " + department);
     }
 
     // Method to validate the password
@@ -101,18 +69,5 @@ public class SignupStaff {
 
         // Password is valid if all conditions are met
         return true;
-    }
-
-    public static void main(String[] args) {
-        // Test the password validation
-        SignupStaff staff = new SignupStaff("John Doe", "john.doe@example.com", "S1234", "IT", "Password123!");
-        
-        staff.displayStaffInfo();
-        
-        if (staff.validatePassword()) {
-            System.out.println("Password is valid.");
-        } else {
-            System.out.println("Password is invalid.");
-        }
     }
 }
